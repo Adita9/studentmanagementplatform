81cfedf3b5007d33f63af3d6247e6cf27ecdbd6d
aneagoe <adrian.neagoe@1and1.ro>
2020-05-30 18:51:18 +0300

    added a component

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,9 +7,11 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import axios from 'axios';
 import BeerList from '@/components/BeerList.vue';
 @Component({
   components: {BeerList}
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+}
 </script>
